c6dcb7da7ec76f715fc6c131d4fbfa19256e776e
CarloPerennes <carlo.perennes@protonmail.com>
2024-11-23 12:07:06 +0100

    ajout de vues
    Ajout des vues pour l'accueil et le hall of fame.

---
+++ core-master/views/accueil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "fr">
+<head>
+    <meta charset = "UTF-8">
+    <title>
+        Escape Game
+    </title>
+</head>
+<body>
+    <h1>Escape Game<h1>
+    <a href = "/escapegame">Jouer</a>
+    <a href = "/halloffame">Hall of fame</a>
+</body>
+</html>
---
+++ core-master/views/halloffame.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "fr">
+<head>
+    <meta charset = "UTF-8">
+    <title>
+        Escape Game
+    </title>
+</head>
+<body>
+    <h1>Hall of fame<h1>
+    <a href = "/escapegame">Jouer</a>
+    <a href = "/accueil">Ecran titre</a>
+</body>
+</html>
