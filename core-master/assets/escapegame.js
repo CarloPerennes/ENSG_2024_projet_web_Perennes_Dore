eaf2cb8f03f1e594153dae1c6dd7779f085ce9f6
CarloPerennes <carlo.perennes@protonmail.com>
2024-11-23 12:08:08 +0100

    update
    Mise à jour permettant de personnaliser le zoom de chaque objet.

--- core-master/assets/escapegame.js
+++ core-master/assets/escapegame.js
@@ -49,11 +49,14 @@ Vue.createApp({
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(the_map);
             
-            var group_marker   = new L.FeatureGroup();
-            var key_group      = new L.FeatureGroup();
-            var chest_group    = new L.FeatureGroup();
             var marker_cadenas = new L.FeatureGroup();
-
+            /*
+            group_marker.addTo(the_map);
+            key_group.addTo(the_map);
+            chest_group.addTo(the_map);
+            marker_cadenas.addTo(the_map);
+            */
+            /*
             the_map.on('zoomend', function(){
                 if (this.getZoom() < 5) {
                     this.removeLayer(group_marker);
@@ -75,7 +78,7 @@ Vue.createApp({
                     this.addLayer(chest_group);
                 }
             });
-
+            */
             marker_cadenas.on('click', (e) => {
                 let marker = e.layer;
                 this.popup_text = marker.objet_texte;
@@ -84,12 +87,9 @@ Vue.createApp({
 
             this.the_map = the_map;
 
-            this.group_marker   = group_marker;
-            this.key_group      = key_group;
-            this.chest_group    = chest_group;
             this.marker_cadenas = marker_cadenas;
             
-            let start_query = "SELECT nom,ST_AsGeoJson(point) AS point,type,icone,inventaire,texte,parent,enfant FROM objet WHERE depart";
+            let start_query = "SELECT nom,ST_AsGeoJson(point) AS point,type,icone,inventaire,texte,parent,enfant,zoom FROM objet WHERE depart";
             let geometry_name = "point";
             
             this.installation(start_query, geometry_name);
@@ -97,75 +97,58 @@ Vue.createApp({
         test: function () {
             console.log("string");
         },
+        
+        display_object: function (object) {
+            geojson_data = {
+                "type": "Feature",
+                "properties": object,
+                "geometry": object.point
+            };
+            var layer = L.geoJson(geojson_data, {
+                pointToLayer: this.createMarker,
+                onEachFeature: this.onEachFeature
+            });
+            var layer_group = new L.FeatureGroup();
+            layer_group.addLayer(layer);
 
-        setSelection: function (object) {
-            console.log("setSelection");
-            console.log(object);
-            this.selected_object = object;
+            if (this.the_map.getZoom() >= object.zoom) {
+                layer_group.addTo(this.the_map);
+            }
+            
+            this.the_map.on('zoomend', function(){
+                if (this.getZoom() < object.zoom) {
+                    this.removeLayer(layer_group);
+                } else {
+                    this.addLayer(layer_group);
+                }
+            });
+
+            this.layer_group[object.nom] = layer_group;
+            /*
+            if (object.type != "cle") {
+                this.chest_group.addLayer(layer_group);
+            } else {
+                this.key_group.addLayer(layer_group);
+            }
+            */
         },
 
         createMarker: function (geoJsonPoint, latlng) {
-            //console.log(geoJsonPoint.properties.icone);
+            let object = geoJsonPoint.properties;
+            //console.log(object.icone);
             let marker = L.marker(latlng, {icon: L.icon({
-                iconUrl: geoJsonPoint.properties.icone,
+                iconUrl: object.icone,
                 iconSize: [64,96],
                 iconAnchor: [31,47],
                 popupAnchor: [0,0]
             })});
-            if (geoJsonPoint.properties.type == "cadenas") {
-                marker.objet_nom = geoJsonPoint.properties.nom;
-                marker.objet_texte = geoJsonPoint.properties.texte;
+            if (object.type == "cadenas") {
+                marker.objet_nom = object.nom;
+                marker.objet_texte = object.texte;
                 marker.bindPopup();
                 marker.addTo(this.marker_cadenas);
             }
             return marker;
-        },
-        
-        clicked_object: function (e) {
-            console.log(e.target);
-            var obj = e.target.feature.properties
-            switch (obj.type) {
-                case "coffre":
-                    //console.log("pas_encore");
-                    //console.log(obj.parent != null && obj.parent == this.selected_object.nom);
-                    if (obj.parent == this.selected_object.nom) {
-                        //console.log("passe");
-                        this.debloque(obj);
-                    }
-                    break;
-                case "cle":
-                    this.liste_obj.push(obj);
-                    this.key_group.removeLayer(this.layer_group[obj.nom]);
-                    break;
-                case "cadenas":
-                    this.cadenas = obj;
-                    break;
-                default:
-                    console.log("default");
-            }
-        },
-
-        submit_function: function () {
-            console.log("submited");
-            let code_try = this.digit1.toString() + this.digit2.toString() + this.digit3.toString() + this.digit4.toString();
-            console.log(this.cadenas.parent == code_try);
-            if (this.cadenas.parent == code_try) {
-                this.debloque(this.cadenas);
-            }
-        },
-
-        popupContent: function (target) {
-            let texte = target.feature.properties.texte;
-            let html = '<p>' + texte + '</p>' +
-                        '<form @submit.prevent="this.submit_function">' +
-                            '<input type="number" id="digit1" name="digit1" min="0" max="9">' +
-                            '<input type="number" id="digit2" name="digit2" min="0" max="9">' +
-                            '<input type="number" id="digit3" name="digit3" min="0" max="9">' +
-                            '<input type="number" id="digit4" name="digit4" min="0" max="9">' +
-                            '<input type="submit" name="envoi" value="OK">' +
-                        '</form>';
-            return html;
-
         },
         
         onEachFeature: function (feature, layer) {
@@ -182,23 +165,59 @@ Vue.createApp({
             });
         },
         
-        display_object: function (object) {
-            geojson_data = {
-                "type": "Feature",
-                "properties": object,
-                "geometry": object.point
-            };
-            var layer = L.geoJson(geojson_data, {
-                pointToLayer: this.createMarker,
-                onEachFeature: this.onEachFeature
-            });
-            this.layer_group[object.nom] = layer;
-
-            if (object.type != "cle") {
-                this.chest_group.addLayer(layer);
-            } else {
-                this.key_group.addLayer(layer);
+        clicked_object: function (e) {
+            console.log(e.target);
+            var obj = e.target.feature.properties
+            switch (obj.type) {
+                case "coffre":
+                    //console.log("pas_encore");
+                    //console.log(obj.parent != null && obj.parent == this.selected_object.nom);
+                    if (obj.parent == this.selected_object.nom) {
+                        //console.log("passe");
+                        this.debloque(obj);
+                    }
+                    break;
+                case "cle":
+                    this.liste_obj.push(obj);
+                    this.layer_group[obj.nom].clearLayers();
+                    //this.the_map.removeLayer(this.layer_group[obj.nom]);
+                    break;
+                case "cadenas":
+                    this.cadenas = obj;
+                    break;
+                default:
+                    console.log("default");
             }
+        },
+
+        setSelection: function (object) {
+            console.log("setSelection");
+            console.log(object);
+            this.selected_object = object;
+        },
+
+        submit_function: function () {
+            console.log("submited");
+            let code_try = this.digit1.toString() + this.digit2.toString() + this.digit3.toString() + this.digit4.toString();
+            console.log(this.cadenas.parent == code_try);
+            if (this.cadenas.parent == code_try) {
+                this.debloque(this.cadenas);
+            }
+        },
+
+        popupContent: function (target) {
+            //This is an old function. It is no more used.
+            let texte = target.feature.properties.texte;
+            let html = '<p>' + texte + '</p>' +
+                        '<form @submit.prevent="submit_function">' +
+                            '<input type="number" id="digit1" name="digit1" min="0" max="9">' +
+                            '<input type="number" id="digit2" name="digit2" min="0" max="9">' +
+                            '<input type="number" id="digit3" name="digit3" min="0" max="9">' +
+                            '<input type="number" id="digit4" name="digit4" min="0" max="9">' +
+                            '<input type="submit" name="envoi" value="OK">' +
+                        '</form>';
+            return html;
+
         },
 
         installation: function (query, geom_name = "") {
@@ -227,37 +246,19 @@ Vue.createApp({
             if (object.enfant == "victory") {
                 this.finish();
             } else {
-            //Fetch request
             console.log("debut debloque");
             //Query def
             let noms_enfant = object.enfant.split(',');
             let def_array = "ARRAY[";
             noms_enfant.forEach(nom => {
-                def_array = def_array + "'" + nom + "'" + ","
+                def_array = def_array + "'" + nom + "'" + ",";
             });
             def_array = def_array.substring(0,def_array.length - 1) + "]";
-            let query = "SELECT nom,ST_AsGeoJson(point) AS point,type,icone,inventaire,texte,parent,enfant FROM objet WHERE nom = ANY (" + def_array + ")";
+            let query = "SELECT nom,ST_AsGeoJson(point) AS point,type,icone,inventaire,texte,parent,enfant,zoom FROM objet WHERE nom = ANY (" + def_array + ")";
             console.log(query);
             let geom_name = "point"
-            let query_def = "query=" + query + "&geom_name=" + geom_name;
-            fetch("/bdd", {
-                method : "post",
-                body : query_def,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-            })
-            .then(result => result.json())
-            .then(result => {
-                //Function
-                console.log("res");
-                console.log(result);
-                result.forEach(element => {
-                    this.display_object(element);
-                });
-                console.log("chest group");
-                console.log(this.chest_group.getLayers());
-            })
+            this.installation(query, geom_name);
+            this.layer_group[object.nom].setPopupContent(object.nom);
         }
         },
 
